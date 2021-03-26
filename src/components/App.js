126794b29a4f4e3b18e5e5e6b6ff8befa10ba6f8
Jorgen <ybeliaev@gmail.com>
2021-03-26 13:04:32 +0200

    add rout for servises $ reorganis it

--- src/components/App.js
+++ src/components/App.js
@@ -1,33 +1,73 @@
 import React from 'react'
-import {BrowserRouter, Route, Switch}  from 'react-router-dom'
+import { BrowserRouter, Route, Switch } from 'react-router-dom'
 
+import Header from './ui/Header'
 
-import Header from "./ui/Header"
-
-import { ThemeProvider } from '@material-ui/core/styles';
-import theme from "./ui/Theme"
-
-
+import { ThemeProvider } from '@material-ui/core/styles'
+import theme from './ui/Theme'
 
 function App() {
-  return (
-    <ThemeProvider theme={theme}>
-      <BrowserRouter>
-        <Header/> 
-      <Switch>
-        <Route exact path="/" component={()=><div>HOME</div>}/>
-        <Route exact path="/services" component={()=><div>SERVICES</div>}/>
-        <Route exact path="/customsoftware" component={()=><div>CUSTOMSOFTWARE</div>}/>
-        <Route exact path="/mobileapps" component={()=><div>MOBILEAPPS</div>}/>
-        <Route exact path="/websites" component={()=><div>WEBSITES</div>}/>
-        <Route exact path="/revolution" component={()=><div>REVOLUTION</div>}/>
-        <Route exact path="/about" component={()=><div>ABOUT</div>}/>
-        <Route exact path="/contact" component={()=><div>CONTACT</div>}/>
-        <Route exact path="/estimate" component={()=><div>ESTIMATE</div>}/>
-      </Switch> 
-      </BrowserRouter>
-    </ThemeProvider>
-  );
+    return (
+        <ThemeProvider theme={theme}>
+            <BrowserRouter>
+                <Header />
+                <Switch>
+                    <Route exact path="/" component={() => <div>HOME</div>} />
+                    <Route
+                        exact
+                        path="/services"
+                        component={() => <div>SERVICES</div>}
+                    />
+                    <Route
+                        exact
+                        path="/soft_dev"
+                        component={() => <div>soft_dev</div>}
+                    />
+                    <Route
+                        exact
+                        path="/modile_dev"
+                        component={() => <div>modile_dev</div>}
+                    />
+                    <Route
+                        exact
+                        path="/website_dev"
+                        component={() => <div>website_dev</div>}
+                    />
+                    <Route
+                        exact
+                        path="/customsoftware"
+                        component={() => <div>CUSTOMSOFTWARE</div>}
+                    />
+                    <Route
+                        exact
+                        path="/mobileapps"
+                        component={() => <div>MOBILEAPPS</div>}
+                    />
+                    <Route
+                        exact
+                        path="/websites"
+                        component={() => <div>WEBSITES</div>}
+                    />
+                    <Route
+                        exact
+                        path="/revolution"
+                        component={() => <div>REVOLUTION</div>}
+                    />
+                    <Route
+                        exact
+                        path="/about"
+                        component={() => <div>ABOUT</div>}
+                    />
+                    <Route
+                        exact
+                        path="/contact"
+                        component={() => <div>CONTACT</div>}
+                    />
+                    {/* <Route exact path="/estimate" component={()=><div>ESTIMATE</div>}/> */}
+                </Switch>
+            </BrowserRouter>
+        </ThemeProvider>
+    )
 }
 
-export default App;
+export default App
--- src/components/ui/Header.js
+++ src/components/ui/Header.js
@@ -1,157 +1,200 @@
-import React, { useEffect } from "react";
-import { Link } from "react-router-dom";
+import React, { useEffect } from 'react'
+import { Link } from 'react-router-dom'
 
-import AppBar from "@material-ui/core/AppBar";
-import Toolbar from "@material-ui/core/Toolbar";
-import Tabs from "@material-ui/core/Tabs";
-import Tab from "@material-ui/core/Tab";
-import useScrollTrigger from "@material-ui/core/useScrollTrigger";
-import { Button } from "@material-ui/core";
-import Menu from "@material-ui/core/Menu";
-import MenuItem from "@material-ui/core/MenuItem";
-import { makeStyles } from "@material-ui/core/styles";
+import AppBar from '@material-ui/core/AppBar'
+import Toolbar from '@material-ui/core/Toolbar'
+import Tabs from '@material-ui/core/Tabs'
+import Tab from '@material-ui/core/Tab'
+import useScrollTrigger from '@material-ui/core/useScrollTrigger'
+import { Button } from '@material-ui/core'
+import Menu from '@material-ui/core/Menu'
+import MenuItem from '@material-ui/core/MenuItem'
+import { makeStyles } from '@material-ui/core/styles'
 
-import logo from "../../assets/logo.svg";
+import logo from '../../assets/logo.svg'
 
 function ElevationScroll(props) {
-  const { children } = props;
+    const { children } = props
 
-  const trigger = useScrollTrigger({
-    disableHysteresis: true,
-    threshold: 0,
-  });
+    const trigger = useScrollTrigger({
+        disableHysteresis: true,
+        threshold: 0,
+    })
 
-  return React.cloneElement(children, {
-    elevation: trigger ? 4 : 0,
-  });
+    return React.cloneElement(children, {
+        elevation: trigger ? 4 : 0,
+    })
 }
 const useStyles = makeStyles((theme) => ({
-  toolbarMargin: {
-    ...theme.mixins.toolbar,
-    marginBottom: "3em",
-  },
-  logo: {
-    height: "8em",
-  },
-  logoContainer: {
-    padding: 0,
-    "&:hover": {
-      backgroundColor: "transparent",
+    toolbarMargin: {
+        ...theme.mixins.toolbar,
+        marginBottom: '3em',
     },
-  },
-  tabContainer: {
-    marginLeft: "auto",
-  },
-  tab: {
-    ...theme.typography.tab,
-    minWidth: 10, // контент кнопок меню ужмётся
-    marginLeft: "25px",
-  },
-}));
+    logo: {
+        height: '8em',
+    },
+    logoContainer: {
+        padding: 0,
+        '&:hover': {
+            backgroundColor: 'transparent',
+        },
+    },
+    tabContainer: {
+        marginLeft: 'auto',
+    },
+    tab: {
+        ...theme.typography.tab,
+        minWidth: 10, // контент кнопок меню ужмётся
+        marginLeft: '25px',
+    },
+}))
 
 export default function Header() {
-  const [value, setValue] = React.useState(0);
-  const [anchorEl, setAnchorEl] = React.useState(null);
-  const [open, setOpen] = React.useState(false);
+    const [value, setValue] = React.useState(0)
+    const [anchorEl, setAnchorEl] = React.useState(null)
+    const [open, setOpen] = React.useState(false)
 
-  const handleClick = (e) => {
-    setAnchorEl(e.currentTarget);
-    setOpen(true);
-  };
-  const handleClose = () => {
-    setAnchorEl(null);
-    setOpen(false);
-  };
-  // чтобы после перезагрузки не сбрасывалось в HOME а оставалось в старом месте
-  useEffect(() => {
-    if (window.location.pathname === "/" && value !== 0) {
-      setValue(0);
-    } else if (window.location.pathname === "/services" && value !== 1) {
-      setValue(1);
-    } else if (window.location.pathname === "/revolution" && value !== 2) {
-      setValue(2);
-    } else if (window.location.pathname === "/about" && value !== 3) {
-      setValue(3);
-    } else if (window.location.pathname === "/contact" && value !== 4) {
-      setValue(4);
+    const handleClick = (e) => {
+        setAnchorEl(e.currentTarget)
+        setOpen(true)
     }
-  }, [value]);
+    const handleClose = () => {
+        setAnchorEl(null)
+        setOpen(false)
+    }
+    // чтобы после перезагрузки не сбрасывалось в HOME а оставалось в старом месте
+    useEffect(() => {
+        if (window.location.pathname === '/' && value !== 0) {
+            setValue(0)
+        } else if (window.location.pathname === '/services' && value !== 1) {
+            setValue(1)
+        } else if (window.location.pathname === '/revolution' && value !== 2) {
+            setValue(2)
+        } else if (window.location.pathname === '/about' && value !== 3) {
+            setValue(3)
+        } else if (window.location.pathname === '/contact' && value !== 4) {
+            setValue(4)
+        }
+    }, [value])
 
-  const handleChange = (event, newValue) => {
-    setValue(newValue);
-  };
-  const classes = useStyles();
-  return (
-    <>
-      <ElevationScroll>
-        <AppBar position="fixed">
-          <Toolbar disableGutters>
-            <Button
-              component={Link}
-              to="/"
-              disableRipple
-              className={classes.logoContainer}
-              onClick={(_) => setValue(0)} // иначе останется выделенным пред пункт меню
-            >
-              <img src={logo} alt="company logo" className={classes.logo} />
-            </Button>
-            <Tabs
-              value={value} // value - порядок пункта меню
-              onChange={handleChange}
-              aria-label="simple tabs example"
-              indicatorColor="primary"
-              className={classes.tabContainer}
-            >
-              <Tab
-                label="Home"
-                className={classes.tab}
-                component={Link}
-                to="/"
-              />
-              <Tab
-                label="Services"
-                aria-controls={anchorEl ? "customized-menu" : undefined}
-                aria-haspopup={anchorEl ? true : undefined}
-                onMouseOver={(e) => handleClick(e)}
-                className={classes.tab}
-                component={Link}
-                to="/services"
-              />
-              <Tab
-                label="The Revolution"
-                className={classes.tab}
-                component={Link}
-                to="/revolution"
-              />
-              <Tab
-                label="About Us"
-                className={classes.tab}
-                component={Link}
-                to="/about"
-              />
-              <Tab
-                label="Contact Us"
-                className={classes.tab}
-                component={Link}
-                to="/contact"
-              />
-            </Tabs>
-            <Menu
-              id="simple-menu"
-              anchorEl={anchorEl}
-              keepMounted
-              open={Boolean(anchorEl)}
-              onClose={handleClose}
-              MenuListProps={{ onMouseLeave: handleClose }} // чтобы скрывалось меню после ухода мышки
-            >
-              <MenuItem onClick={handleClose}>Software development</MenuItem>
-              <MenuItem onClick={handleClose}>Mobile App development</MenuItem>
-              <MenuItem onClick={handleClose}>Website development</MenuItem>
-            </Menu>
-          </Toolbar>
-        </AppBar>
-      </ElevationScroll>
-      <div className={classes.toolbarMargin} />
-    </>
-  );
+    const handleChange = (event, newValue) => {
+        setValue(newValue)
+    }
+    const classes = useStyles()
+    return (
+        <>
+            <ElevationScroll>
+                <AppBar position="fixed">
+                    <Toolbar disableGutters>
+                        <Button
+                            component={Link}
+                            to="/"
+                            disableRipple
+                            className={classes.logoContainer}
+                            onClick={(_) => setValue(0)} // иначе останется выделенным пред пункт меню
+                        >
+                            <img
+                                src={logo}
+                                alt="company logo"
+                                className={classes.logo}
+                            />
+                        </Button>
+                        <Tabs
+                            value={value} // value - порядок пункта меню
+                            onChange={handleChange}
+                            aria-label="simple tabs example"
+                            indicatorColor="primary"
+                            className={classes.tabContainer}
+                        >
+                            <Tab
+                                label="Home"
+                                className={classes.tab}
+                                component={Link}
+                                to="/"
+                            />
+                            <Tab
+                                label="Services"
+                                aria-controls={
+                                    anchorEl ? 'customized-menu' : undefined
+                                }
+                                aria-haspopup={anchorEl ? true : undefined}
+                                onMouseOver={(e) => handleClick(e)}
+                                className={classes.tab}
+                                component={Link}
+                                to="/services"
+                            />
+                            <Tab
+                                label="The Revolution"
+                                className={classes.tab}
+                                component={Link}
+                                to="/revolution"
+                            />
+                            <Tab
+                                label="About Us"
+                                className={classes.tab}
+                                component={Link}
+                                to="/about"
+                            />
+                            <Tab
+                                label="Contact Us"
+                                className={classes.tab}
+                                component={Link}
+                                to="/contact"
+                            />
+                        </Tabs>
+                        <Menu
+                            id="simple-menu"
+                            anchorEl={anchorEl}
+                            keepMounted
+                            open={Boolean(anchorEl)}
+                            onClose={handleClose}
+                            MenuListProps={{ onMouseLeave: handleClose }} // чтобы скрывалось меню после ухода мышки look https://material-ui.com/api/menu/#main-content
+                        >
+                            <MenuItem
+                                onClick={() => {
+                                    handleClose()
+                                    setValue(1)
+                                }}
+                                component={Link}
+                                to="/services"
+                            >
+                                Services
+                            </MenuItem>
+                            <MenuItem
+                                onClick={() => {
+                                    handleClose()
+                                    setValue(1)
+                                }}
+                                component={Link}
+                                to="/soft_dev"
+                            >
+                                Software development
+                            </MenuItem>
+                            <MenuItem
+                                onClick={() => {
+                                    handleClose()
+                                    setValue(1)
+                                }}
+                                component={Link}
+                                to="/modile_dev"
+                            >
+                                Mobile App development
+                            </MenuItem>
+                            <MenuItem
+                                onClick={() => {
+                                    handleClose()
+                                    setValue(1)
+                                }}
+                                component={Link}
+                                to="/website_dev"
+                            >
+                                Website development
+                            </MenuItem>
+                        </Menu>
+                    </Toolbar>
+                </AppBar>
+            </ElevationScroll>
+            <div className={classes.toolbarMargin} />
+        </>
+    )
 }
